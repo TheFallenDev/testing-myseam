d144d5581fe1bf39bdaf1f3871bca8a2ff136c70
Ignacio Waigandt <ignaawaig@gmail.com>
2023-04-18 20:55:04 -0300

    arreglos

--- src/controllers/paymentControllers.js
+++ src/controllers/paymentControllers.js
@@ -1,41 +1,44 @@
-const mercadopago = require('mercadopago');
-import { User } from "../db.js";
-import axios from 'axios';
+const mercadopago = require("mercadopago");
+const { User } = require("../db.js");
+const axios = require('axios');
 
-export async function createPreference(items, seller_id) {
-  mercadopago.configure({
-    access_token: process.env.ACCESS_TOKEN,
-  });
-  // Crea un objeto de preferencia
-  let preference = {
-    back_urls: {
-      success: "https://my-seam.vercel.app/",
-      failure: "http://localhost:3000/checkout/failure",
-      pending: "http://localhost:3000/checkout/pending",
-    },
-    auto_return: "approved",
-    items: [],
-  };
-  items.forEach((item) => {
-    preference.items.push(item);
-  });
-  const seller = await User.findByPk(seller_id);
-  // Enviar solicitud de preferencia
-  axios({
-    method: "POST",
-    url: "https://api.mercadopago.com/checkout/preferences",
-    headers: {
-      Authorization: `Bearer ${seller.MPAccessToken}`,
-      "Content-Type": "application/json",
-    },
-    data: preference,
-  })
-    .then(function (response) {
-      res.json({
-        global: response.body.id,
-      });
+module.exports = {
+  createPreference: async (items, seller_id) => {
+    mercadopago.configure({
+      access_token: process.env.ACCESS_TOKEN,
     })
-    .catch(function (error) {
-      console.log(error);
+    // Crea un objeto de preferencia
+    let preference = {
+      back_urls: {
+        success: "https://my-seam.vercel.app/",
+        failure: "http://localhost:3000/checkout/failure",
+        pending: "http://localhost:3000/checkout/pending",
+      },
+      auto_return: "approved",
+      items: [],
+    };
+    items.forEach((item) => {
+      preference.items.push(item);
     });
-}
+    const seller = await User.findByPk(seller_id);
+    // Enviar solicitud de preferencia
+    axios({
+      method: "POST",
+      url: "https://api.mercadopago.com/checkout/preferences",
+      headers: {
+        Authorization: `Bearer ${seller.MPAccessToken}`,
+        "Content-Type": "application/json", 
+      },
+      data: preference,
+    })
+      .then(function (response) {
+        console.log(response);
+        res.json({
+          global: response.body.id,
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  },
+};
